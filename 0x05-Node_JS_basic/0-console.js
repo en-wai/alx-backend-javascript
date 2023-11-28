e29dbb38e161b35f4059b01960a5184264ba2d25
en-wai <nyasare@hotmail.com>
2023-11-28 11:44:23 +0000

    Solve Basic Node Projects 0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,16 @@
+// Algorithm
+
+/* Define the func called displayMessage*/
+
+/* Display a message func is called, console.log whatever text is is passed as
+a paramter*/
+
+
+// CODE
+
+function displayMessage(message){
+    console.log(message);
+
+module.exports = displayMessage;
+}
+
